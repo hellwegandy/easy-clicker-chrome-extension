b58f4f9fa4635623047144f6925059085f188c41
hellwegandy <hellwegandy@gmail.com>
2018-05-26 18:14:36 -0500

    added comments

--- popup.js
+++ popup.js
@@ -1,19 +1,27 @@
 // popup.js
+
+// Function ran on query submit button click
 function clickyClick(){
-    chrome.tabs.query({active: true, currentWindow: true}, findQueryMatch);
-}
-function findQueryMatch(tabs) {
-    let activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "click_query"});
+    sendQueryMessage()
 }
 
+// finds the active tab and runs the query against that tab
+function sendQueryMessage(){
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+        let activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "click_query", "query": 'blah blah'});
+    });
+}
+
+// set on click of query submit button
 let myFunctionButton = document.getElementById("myFunctionButton");
 myFunctionButton.onclick = clickyClick;
 
+// event listener to tell us how many items were clicked
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "query_finished" ) {
-            alert('clicked: ' + request.clicked);
+            console.log('clicked: ' + request.clicked);
         }
     }
 );
